4a84c29da5fcdb53962622116cad9d8ee0ad6a99
urii36 <kazan313131@gmail.com>
2023-06-05 10:58:35 +0300

    calling the subtraction function

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from funcs import sum_numbers
+from funcs import sum_numbers, subtraction_func
 
 sum_numbers()
-print(sum_numbers())
+#print(sum_numbers())
+
+subtraction_func()
+#print(subtraction_func())
+
